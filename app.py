2b033f21d0ea3bfcd4b436f54406a4ba9aed12bf
Harshal <harshal3558@gmail.com>
2025-08-24 23:55:11 +0530

    model_trainer done

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from src.LAP.components.data_ingestion import DataIngestion
 from src.LAP.components.data_ingestion import DataIngestionConfig
 from src.LAP.components.data_transformation import DataTransformation
 from src.LAP.components.data_transformation import DataTransformationConfig
-# from src.LAP.components.model_tranier import ModelTrainerConfig
-# from src.LAP.components.model_tranier import ModelTrainer
+from src.LAP.components.model_tranier import ModelTrainerConfig
+from src.LAP.components.model_tranier import ModelTrainer
 
 import sys
 
@@ -20,13 +20,13 @@ if __name__ == "__main__":
 
         # data_transformation_config=DataIngestionConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-        # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        # data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         
         # ## Model Training
 
-        # model_trainer=ModelTrainer()
-        # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
     except Exception as e:
