ef578565fdbe181ac011b152f68a4446debef9f8
Harshal <harshal3558@gmail.com>
2026-01-02 21:06:32 +0530

    files are updated

--- test_app.py
+++
@@ -1,7 +0,0 @@
-from application import app
-
-def test_home():
-    response=app.test_client().get("/")
-
-    assert response.status_code==200
-    assert response.data== b"Hello World!"
---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+from application import app
+
+def test_home_route():
+    client = app.test_client()
+    response = client.get("/hello")
+    assert response.status_code == 200
